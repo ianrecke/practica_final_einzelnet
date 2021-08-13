ef4bbc84e1db09f34f66eac426863d1231c462ba
ianrecke <ian.recke@gmail.com>
2021-08-13 13:10:11 +0200

    prueba_lectura_confirmed

--- script.py
+++ script.py
@@ -20,6 +20,7 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
+    confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
     return flask.render_template('index.html')
 
 def daily_increase(data):
