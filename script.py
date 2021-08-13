87eb2bb5a9a298b74a9b80cc1492e53a778d8154
ianrecke <ian.recke@gmail.com>
2021-08-13 12:29:00 +0200

    script

--- script.py
+++ script.py
@@ -18,74 +18,7 @@ app=Flask(__name__)
 def index():
     return flask.render_template('index.html')
 
-def daily_increase(data):
-    d = [] 
-    for i in range(len(data)):
-        if i == 0:
-            d.append(data[0])
-        else:
-            d.append(data[i]-data[i-1])
-    return d 
 
-def moving_average(data, window_size):
-    moving_average = []
-    for i in range(len(data)):
-        if i + window_size < len(data):
-            moving_average.append(np.mean(data[i:i+window_size]))
-        else:
-            moving_average.append(np.mean(data[i:len(data)]))
-    return moving_average
-
-def entrenamientoModelo(X_train_confirmed,X_test_confirmed,future_forcast):
-    poly = PolynomialFeatures(degree=4)
-    poly_X_train_confirmed = poly.fit_transform(X_train_confirmed)
-    poly_X_test_confirmed = poly.fit_transform(X_test_confirmed)
-    poly_future_forcast = poly.fit_transform(future_forcast)
-    linear_model = LinearRegression(normalize=True, fit_intercept=False)
-    linear_model.fit(poly_X_train_confirmed, y_train_confirmed)
-    return linear_model
-    
-def obtencion_datos():
-    confirmed_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
-    deaths_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
-    # recoveries_df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv')
-    latest_data = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/08-06-2021.csv')
-    
-    confirmed = confirmed_df.loc[:, cols[4]:cols[-1]]
-    deaths = deaths_df.loc[:, cols[4]:cols[-1]]
-    dates = confirmed.keys()
-    world_cases = []
-    total_deaths = [] 
-    mortality_rate = []
-    for i in dates:
-        confirmed_sum = confirmed[i].sum()
-        death_sum = deaths[i].sum()
-        world_cases.append(confirmed_sum)
-        total_deaths.append(death_sum)
-        mortality_rate.append(death_sum/confirmed_sum)
-        
-    window = 7
-
-    # confirmed cases
-    world_daily_increase = daily_increase(world_cases)
-    world_confirmed_avg= moving_average(world_cases, window)
-    world_daily_increase_avg = moving_average(world_daily_increase, window)
-
-    # deaths
-    world_daily_death = daily_increase(total_deaths)
-    world_death_avg = moving_average(total_deaths, window)
-    world_daily_death_avg = moving_average(world_daily_death, window)
-    
-    days_since_1_22 = np.array([i for i in range(len(dates))]).reshape(-1, 1)
-    world_cases = np.array(world_cases).reshape(-1, 1)
-    total_deaths = np.array(total_deaths).reshape(-1, 1)
-
-    days_in_future = 10
-    future_forcast = np.array([i for i in range(len(dates)+days_in_future)]).reshape(-1, 1)
-    adjusted_dates = future_forcast[:-10]
-    X_train_confirmed, X_test_confirmed, y_train_confirmed, y_test_confirmed = train_test_split(days_since_1_22[50:], world_cases[50:], test_size=0.03, shuffle=False) 
-    modelo = entrenamientoModelo(X_train_confirmed,X_test_confirmed,future_forcast)
-    
     
     
 @app.route('/result',methods = ['POST'])
