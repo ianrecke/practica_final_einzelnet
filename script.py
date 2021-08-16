35d945325fc03f5d1ecd909780154755115f083c
ianrecke <ian.recke@gmail.com>
2021-08-16 12:16:39 +0200

    script_train_modelo_9

--- script.py
+++ script.py
@@ -112,7 +112,7 @@ def result():
         to_predict_list = list(to_predict_list.values())
 #         result = predicciones(to_predict_list[0],dates)
         fecha = to_predict_list[0].split(sep = '-')
-        fecha = fecha[1]+"/"+fecha[0]+'/'+fecha[2]
+        fecha = fecha[1]+"/"+fecha[2]+'/'+fecha[0]
         copia_entrada = to_predict_list
         try:
             to_predict_list = list(map(float, to_predict_list))
