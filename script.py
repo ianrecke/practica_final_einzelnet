dddc65ce9930b78beaffa2008996facced30a1d7
ianrecke <ian.recke@gmail.com>
2021-08-16 13:15:31 +0200

    script_train_modelo_20

--- script.py
+++ script.py
@@ -123,7 +123,7 @@ def result():
         copia_entrada = to_predict_list
         try:
             to_predict_list = list(map(float, to_predict_list))
-            if result < 0:
+            if result[0][0] < 0:
                 prediction = '<p style="color: green">El numero de casos de covid a nivel Global disminuye en un '+result+' para la fecha indicada</p>'
             else:
                 prediction = '<p style="color: red">El numero de casos de covid a nivel Global aumenta en un '+result+' para la fecha indicada</p>'
